d2fcbf5aa8b13c425085c924c5711480fbb709e0
sabmeua <sabme.ua@gmail.com>
2018-12-10 05:36:52 +0000

    Add mem trace

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <stdlib.h>
 #include "mbed.h"
+#include "mbed_mem_trace.h"
 
-int main()
-{
+int main() {
+    mbed_mem_trace_set_callback(mbed_mem_trace_default_callback);
+    while (true) {
+        void *p = malloc(50);
+        wait(0.5);
+        free(p);
+    }
 }
 
