0a1669c33f17cfda223ba31fb67a8ad9d732f0c4
sabmeua <sabme.ua@gmail.com>
2018-12-14 09:01:51 +0000

    Allow unaligned access in SDRAM

--- main.cpp
+++ main.cpp
@@ -16,6 +16,34 @@ extern "C" caddr_t _sbrk(int incr) {
 
     if (isSdramInitialized == 0) {
         BSP_SDRAM_Init();
+
+        /**
+         * Allow unaligned access in SDRAM by configure MPU
+         * -- http://www.keil.com/support/docs/3777.htm
+         */
+        MPU_Region_InitTypeDef MPU_InitStruct;
+
+        /* Disable the MPU */
+        HAL_MPU_Disable();
+
+        /* Configure the MPU attributes for SDRAM */
+        MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+        MPU_InitStruct.BaseAddress = 0xC0000000;
+        MPU_InitStruct.Size = MPU_REGION_SIZE_8MB;
+        MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+        MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+        MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+        MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+        MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+        MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+        MPU_InitStruct.SubRegionDisable = 0x00;
+        MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+
+        HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+        /* Enable the MPU */
+        HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
         isSdramInitialized = 1;
     }
 
